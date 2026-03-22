f9f02395661ea1eba24676bc980743337e01c5b7
Anna <haritonova1512@gmail.com>
2026-03-22 23:32:55 +0200

    rename onChange

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -2,17 +2,17 @@ import css from "./SearchBox.module.css";
 
 interface SearchBoxProps {
   value: string;
-  onSubmit: (value: string) => void;
+  onChange: (value: string) => void;
 }
 
-function SearchBox({ value, onSubmit }: SearchBoxProps) {
+function SearchBox({ value, onChange }: SearchBoxProps) {
   return (
     <input
       className={css.input}
       type="text"
       placeholder="Search notes"
       value={value}
-      onChange={(e) => onSubmit(e.target.value)}
+      onChange={(e) => onChange(e.target.value)}
     />
   );
 }
